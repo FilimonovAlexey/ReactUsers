a2658c734002bad884c5f42aa8528ddf040edb7d
Алексей Филимонов <alexeyf08@gmail.com>
2023-01-05 16:28:36 +0700

    feat: рабочая версия

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ import { Users } from './components/Users';
 function App() {
 
   const [users, setUsers] = React.useState([]);
+  const [invites, setInvites] = React.useState([]);
   const [isLoading, setLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [success, setSuccess] = React.useState(false);
 
   React.useEffect(() => {
     fetch('https://reqres.in/api/users')
@@ -23,10 +26,37 @@ function App() {
     .finally(() => setLoading(false));
   }, []);
 
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  }
+
+const onClickInvite = (id) => {
+  if (invites.includes(id)) {
+    setInvites(prev => prev.filter(_id => _id !== id))
+  } else {
+    setInvites((prev) => [...prev, id]);
+  }
+}
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  }
+
   return (
     <div className="App">
-      <Users items={users} isLoading={isLoading}/>
-      {/* <Success /> */}
+      {success ? (
+        <Success count={invites.length} /> 
+      ) : (
+        <Users 
+        onChangeSearchValue={onChangeSearchValue}
+        searchValue={searchValue} 
+        items={users} 
+        isLoading={isLoading}
+        invites={invites}
+        onClickInvite={onClickInvite}
+        onClickSendInvites={onClickSendInvites}
+        />
+      )}
     </div>
   );
 }
